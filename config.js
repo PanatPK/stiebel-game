da9aec6d25a504bbb0860510088c98a1caa1fb12
PanatPK <panatchaik@gmail.com>
2026-09-16 23:04:05 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3XWmB71uM2UqD8iFyLjLdqsk2uP6mb3o",
+  authDomain: "ptt-vote.firebaseapp.com",
+  databaseURL: "https://ptt-vote-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ptt-vote",
+  appId: "1:169658318025:web:067d4ec892c1bbeab7a118"
+};
+const ADMIN_PIN = "0610";
+const EVENT_NAME = "THE X-CLUSIVE NIGHT";
